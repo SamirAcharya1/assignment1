a2efab4c6d9720198e9c6cff30f70ec1e08d4176
SamirAcharya1 <158140847+SamirAcharya1@users.noreply.github.com>
2025-04-04 15:53:11 +0000

    Closing it off

--- attendance_project/attendance_project/urls.py
+++ attendance_project/attendance_project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("attendance/", include("attendance.urls")),
+    path("", include("attendance.urls")),
 ]
